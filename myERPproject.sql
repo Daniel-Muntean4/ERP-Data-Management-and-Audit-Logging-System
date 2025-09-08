138a3e7b7a7158ea2937f18b123973cde161f913
Daniel Muntean <125653791+Daniel-Muntean4@users.noreply.github.com>
2025-09-08 13:27:30 +0300

    Update myERPproject.sql
    Added data selection of department by month and modified users table to user

--- myERPproject.sql
+++ myERPproject.sql
@@ -2,14 +2,15 @@ CREATE TABLE IF NOT EXISTS Roles(
     role_id INT PRIMARY KEY,
     role_name VARCHAR(50) NOT NULL,
 );
+  
 LOAD DATA LOCAL INFILE '/Users/daniel/Downloads/roles.csv'
-    INTO TABLE your_table_name
+    INTO TABLE Roles
     FIELDS TERMINATED BY ','
     ENCLOSED BY '"'
     LINES TERMINATED BY '\n'
     IGNORE 1 ROWS;
 
-CREATE TABLE IF NOT EXISTS Users(
+CREATE TABLE IF NOT EXISTS User(
     user_id INT PRIMARY KEY,
     username VARCHAR(50),
     full_name VARCHAR(50),
@@ -18,7 +19,22 @@ CREATE TABLE IF NOT EXISTS Users(
 );
   
 LOAD DATA LOCAL INFILE '/Users/daniel/Downloads/users.csv'
-    INTO TABLE your_table_name
+    INTO TABLE User
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 ROWS;
+
+CREATE TABLE IF NOT EXISTS Audit_log(
+  log_id INT PRIMARY KEY, 
+  user_id INT foreign KEY,
+  action_type VARCHAR(50),
+  table_name VARCHAR(50),
+  record_id NUMERIC(5,1),
+  timestamp timestamp
+)
+LOAD DATA LOCAL INFILE '/Users/daniel/Downloads/audit_log.csv'
+    INTO TABLE Audit_log
     FIELDS TERMINATED BY ','
     ENCLOSED BY '"'
     LINES TERMINATED BY '\n'
@@ -43,7 +59,7 @@ CREATE TABLE IF NOT EXISTS User_roles(
   user_role_id INT AUTO_INCREMENT PRIMARY KEY , 
   user_id INT,
   role_id INT,
-  foreign KEY(user_id) references Users(user_id),
+  foreign KEY(user_id) references User(user_id),
   foreign KEY(role_id) references Roles(role_id)
 )
 LOAD DATA LOCAL INFILE '/Users/daniel/Downloads/user_roles.csv'
@@ -53,7 +69,7 @@ LOAD DATA LOCAL INFILE '/Users/daniel/Downloads/user_roles.csv'
     LINES TERMINATED BY '\n'
     IGNORE 1 ROWS;
 
-SELECT * FROM Users
+SELECT * FROM User
 SELECT * FROM Roles
 SELECT * FROM User_roles
 SELECT * FROM Audit_log
@@ -74,20 +90,19 @@ DELETE FROM Audit_log
   WHERE log_id IN (SELECT log_id  FROM CTE_DUPLICATES WHERE ROW_NUM>1)
   
 SELECT TRIM(SUBSTRING(FULL_NAME,1, LOCATE(' ',FULL_NAME))) 
-  FROM USERS
+  FROM USER
+  
 SELECT SUBSTRING(FULL_NAME, LOCATE(' ',FULL_NAME)+1)
-  FROM USERS
+  FROM USER
 
-  ALTER TABLE Users
-  ADD first_name VARCHAR(50)
+  ALTER TABLE User
+  ADD first_name VARCHAR(50),
+  ADD last_name VARCHAR(50);
 
-  UPDATE USERS  
-  SET first_name=TRIM(SUBSTRING(FULL_NAME,1, LOCATE(' ',FULL_NAME)))
-  last_name=SUBSTRING(FULL_NAME, LOCATE(' ',FULL_NAME)+1)
-  
-  ALTER TABLE Users
-  ADD last_name VARCHAR(50)
-  
+  UPDATE USER
+  SET first_name=TRIM(SUBSTRING(FULL_NAME,1, LOCATE(' ',FULL_NAME))),
+  last_name=SUBSTRING(FULL_NAME, LOCATE(' ',FULL_NAME)+1);
+    
 SELECT Audit_log_empty.action_type, Audit_log_empty.table_name, Audit_log_VALUE.action_type, Audit_log_value.table_name
 FROM Audit_log Audit_log_empty
 JOIN Audit_log Audit_log_value
@@ -103,14 +118,14 @@ WHERE Audit_log_value.table_name IS NULL
 
 -- SELECTING THE EMPLOYEES THAT MADE AN ACTION ACTION OUTSIDE OF WORKING TIME
 SELECT U.username, L.action_type, COUNT(L.timestamp) from Audit_log l
-JOIN USERS u
+JOIN USER u
 ON l.user_id=u.user_id
 where EXTRACT( HOUR FROM timestamp ) not BETWEEN 8 AND 18
 GROUP BY U.username, L.action_type
 ORDER BY U.username, L.action_type
 
 SELECT username, COUNT(action_type) from Audit_log l
-JOIN USERS u
+JOIN USER u
 ON l.user_id=u.user_id
 where EXTRACT( HOUR FROM timestamp ) not BETWEEN 8 AND 18
 group by username
@@ -120,16 +135,36 @@ SELECT distinct(username) from users
    JOIN Audit_log
 ON users.user_id=Audit_log.user_id
 WHERE is_active=0
-  
-  WITH DAILY_ACTIONS AS (
+
 SELECT EXTRACT(YEAR FROM timestamp) AS year, EXTRACT(MONTH FROM timestamp) AS month, 
-  EXTRACT(DAY FROM timestamp) AS day, count(action_type) as counts
+  EXTRACT(DAY FROM timestamp) AS day, count(action_type), (count(action_type)) OVER(), EXTRACT(MONTH FROM timestamp), 
+  EXTRACT(DAY FROM timestamp))
 from Audit_log
 group by EXTRACT(YEAR FROM timestamp), EXTRACT(MONTH FROM timestamp), EXTRACT(DAY FROM timestamp)
-order by EXTRACT(YEAR FROM timestamp), EXTRACT(MONTH FROM timestamp), EXTRACT(DAY FROM timestamp))
+order by EXTRACT(YEAR FROM timestamp), EXTRACT(MONTH FROM timestamp), EXTRACT(DAY FROM timestamp)
 
-SELECT year, month, day, counts, (SELECT avg(counts) FROM DAILY_ACTIONS) AS AVG_ACTIONS from daily_actions
-WHERE counts<(SELECT avg(counts) FROM DAILY_ACTIONS)
+SELECT user.user_id, user.full_name, MIN(user.deleted_at) deletion_time, MIN(Audit_log.timestamp) action_time FROM USER
+  JOIN Audit_log
+  WHERE Audit_log.timestamp>user.deleted_at
+GROUP BY user.user_id, user.full_name
+
+  WITH USER_role_name AS (
+WITH user_role AS (
+SELECT user.user_id, USER.username, USER.department, USER_ROLES.role_id FROM user
+JOIN User_roles
+ON user.user_id=User_roles.user_id)
+
+SELECT user_id, username, department, role_name FROM user_role 
+JOIN roles 
+ON user_role.role_id=roles.role_id
+ORDER BY DEPARTMENT, USER_ID)
+
+SELECT user_role_name.department, EXTRACT(YEAR FROM timestamp) as year, EXTRACT(MONTH FROM timestamp) as month,  
+  COUNT(action_type) as number_actions,
+  RANK() OVER(PARTITION BY EXTRACT(MONTH FROM timestamp) ORDER BY COUNT(action_type) DESC) FROM Audit_log
+JOIN user_role_name
+ON Audit_log.user_id=user_role_name.user_id
+group by user_role_name.department, EXTRACT(YEAR FROM timestamp), EXTRACT(MONTH FROM timestamp)
 
 
 
